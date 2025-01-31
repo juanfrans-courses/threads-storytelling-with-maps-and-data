d0a5f2c3b226093dd6a6b251f59c600e51fb7b78
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2025-01-30 22:46:41 -0500

    updated site

---
+++ 01_Work_Environments_and_Personal_Site/01_PersonalSiteDemo/Assignment_01/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianVhbmZyYW5zIiwiYSI6Imdhb2V1S1UifQ._00SXA6yc6U6CJG-JI4fiA';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/juanfrans/cm6jjlhqn00wx01qm0ez1hond',
+    zoom: 10,
+    center: [-74, 40.725]
+});
