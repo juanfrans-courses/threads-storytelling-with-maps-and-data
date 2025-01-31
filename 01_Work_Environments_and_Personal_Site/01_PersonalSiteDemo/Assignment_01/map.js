810afafe59a33e621cd22317f9dd9cedf8039b28
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2025-01-30 22:52:39 -0500

    removed style url and access token

--- 01_Work_Environments_and_Personal_Site/01_PersonalSiteDemo/Assignment_01/map.js
+++ 01_Work_Environments_and_Personal_Site/01_PersonalSiteDemo/Assignment_01/map.js
@@ -1,7 +1,7 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoianVhbmZyYW5zIiwiYSI6Imdhb2V1S1UifQ._00SXA6yc6U6CJG-JI4fiA';
+mapboxgl.accessToken = 'Add your access token here';
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/juanfrans/cm6jjlhqn00wx01qm0ez1hond',
+    style: 'Add your style URL here',
     zoom: 10,
     center: [-74, 40.725]
 });
